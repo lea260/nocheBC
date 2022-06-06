83637c463909565b1dbf17f03f565e1bb41546f1
leandro <lcalvetti1@gmail.com>
2022-06-06 20:39:07 -0300

    agregado de articulos

--- config/config.php
+++ config/config.php
@@ -5,8 +5,8 @@ define('URL', 'http://' . $_SERVER['HTTP_HOST'] . '/nocheBC/');
 
 //conexion a la base de datos
 define('HOST', 'localhost');
-define('PORT', '3306');
-define('DB', 'base03');
+define('PORT', '3307');
+define('DB', 'nocheBC');
 define('USER', 'usuario03');
 define('PASSWORD', "usuario03");
 define('CHARSET', 'utf8mb4');
