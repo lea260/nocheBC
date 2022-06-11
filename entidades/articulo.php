1968858c544a8f4cad1e38aec5a84856cc938753
Gonzalo Fernandez <Gonzalo Fernandez>
2022-06-10 21:27:49 -0300

    mm

---
+++ entidades/articulo.php
@@ -0,0 +1,14 @@
+<?php
+
+class Articulo
+{
+    public $id;
+    //no va a ser usado
+    public $nombre;
+    //no va a ser usado
+    public $codigo;
+    public $descripcion;
+    public $precio;
+    public $fecha;
+    public $url;
+}
