1a29922bedd0457a6e801e3a8dc9f25012bf9ab1
leandro <lcalvetti1@gmail.com>
2022-06-13 20:45:26 -0300

    agregar

--- controllers/Articulos_Controller.php
+++ controllers/Articulos_Controller.php
@@ -20,8 +20,14 @@ class Articulos_Controller extends Controller
     public function nuevo()
     {
         //$alumnos = $this->model->get();
-        $this->view->alumnos = "cargado";
+        //$this->view->alumnos = "cargado";
         $this->view->render('articulos/nuevo');
+    }
+    public function crear()
+    {
+        $this->view->lista = $this->model->listar();
+        //$this->view->lista = ["articulo01", "articulo02"];
+        $this->view->render('articulos/listar');
     }
     public function listar()
     {
