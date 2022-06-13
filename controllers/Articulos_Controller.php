b6179e983b242e327d407a86ebbb6847d9016d70
clavijo <clavijo>
2022-06-13 20:27:42 -0300

    agregue formulario crear articulo

--- controllers/Articulos_Controller.php
+++ controllers/Articulos_Controller.php
@@ -16,6 +16,21 @@ class Articulos_Controller extends Controller
         $this->view->alumnos = "cargado";
         $this->view->render('login/index');
     }
+
+    public function nuevo()
+    {
+        //$alumnos = $this->model->get();
+        $this->view->alumnos = "cargado";
+        $this->view->render('articulos/nuevo');
+    }
+
+    public function crear()
+    {
+        $this->view->lista = $this->model->listar();
+        //$this->view->lista = ["articulo01", "articulo02"];
+        $this->view->render('articulos/crear');
+    }
+
     public function listar()
     {
         $this->view->lista = $this->model->listar();
