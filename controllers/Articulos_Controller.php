689c2b2abb840986d87a422381217189493814f6
Geronimo <gerocgon@gmail.com>
2022-06-13 20:09:26 -0300

    si

--- controllers/Articulos_Controller.php
+++ controllers/Articulos_Controller.php
@@ -12,10 +12,18 @@ class Articulos_Controller extends Controller
     //base+
     public function render()
     {
-        $alumnos             = $this->model->listar();
+        //$alumnos             = $this->model->listar();
         $this->view->alumnos = "cargado";
         $this->view->render('login/index');
     }
+
+    public function nuevo()
+    {
+        //$alumnos             = $this->model->listar();
+        $this->view->alumnos = "cargado";
+        $this->view->render('articulos/nuevo');
+    }
+
     public function listar()
     {
         $this->view->lista = $this->model->listar();
