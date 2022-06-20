1d317e789253f8a5e660db0ea29a668f0c2ec52b
Geromino <gerocgon@gmail.com>
2022-06-20 20:29:41 -0300

    agregue login.php

--- controllers/Login_Controller.php
+++ controllers/Login_Controller.php
@@ -1,13 +1,13 @@
 <?php
-require_once 'vendor/autoload.php';
-require_once 'auth/Auth.php';
+//require_once 'vendor/autoload.php';
+//require_once 'auth/Auth.php';
 
 class Login_Controller extends Controller
 {
     public function __construct()
     {
         parent::__construct();
-        $this->view->mensaje = "";
+        $this->view->mensaje        = "";
         $this->view->resultadoLogin = "";
     }
 
@@ -21,8 +21,8 @@ class Login_Controller extends Controller
 
     public function ingresar()
     {
-        $nombre = $_POST['nombre'];
-        $pass = $_POST['pass'];
+        $nombre     = $_POST['nombre'];
+        $pass       = $_POST['pass'];
         $exitoLogin = $this->model->ingresar($nombre, $pass);
         if ($exitoLogin) {
             $token = Auth::SignIn([
@@ -30,10 +30,10 @@ class Login_Controller extends Controller
                 'name' => $nombre,
                 'role' => 'cliente',
             ]);
-            $this->view->token = $token;
+            $this->view->token        = $token;
             $_SESSION["estalogueado"] = true;
-            $_SESSION["nombre"] = $nombre;
-            $_SESSION["rol"] = "cliente";
+            $_SESSION["nombre"]       = $nombre;
+            $_SESSION["rol"]          = "cliente";
             $this->view->render('login/ingresar');
         } else {
             $this->view->resultadoLogin = "usuario o contraseña incorrectos";
@@ -49,5 +49,10 @@ class Login_Controller extends Controller
         session_destroy();
         $this->view->render('index/index');
 
+    }
+
+    public function login()
+    {
+        $this->view->render('login/login');
     }
 }
