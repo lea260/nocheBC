a6e08a45698cf50451a3cc4b0d75da811731fbb8
Geromino <gerocgon@gmail.com>
2022-07-29 19:54:38 -0300

    clase 29 07

--- config/config.php
+++ config/config.php
@@ -6,7 +6,7 @@ define('URL', 'http://' . $_SERVER['HTTP_HOST'] . '/nocheBC/');
 //conexion a la base de datos
 define('HOST', 'localhost');
 define('PORT', '3307');
-define('DB', 'nochebcv01');
+define('DB', 'nochebcv10');
 define('USER', 'root');
 define('PASSWORD', "");
 define('CHARSET', 'utf8mb4');
---
+++ controllers/Apiarticulos_Controller.php
@@ -0,0 +1,34 @@
+<?php
+
+class Apiarticulos_Controller extends Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->view->mensaje        = "";
+        $this->view->resultadoLogin = "";
+    }
+
+    //base+login
+    public function render()
+    {
+        //$alumnos = $this->model->get();
+
+        $this->view->render('articulos/index');
+    }
+    public function listar()
+    {
+        //$alumnos = $this->model->get();
+
+        $json           = file_get_contents('php://input');
+        $lista          = $this->model->listar();
+        $listaArticulos = json_decode($json);
+        $respuesta      = [
+            "articulos" => $lista,
+            "Total" => count($lista),
+        ];
+        $this->view->respuesta = json_encode($respuesta);
+        $this->view->render('apiarticulos/listar');
+    }
+
+}
