237fa7ff07490193703cd8baad1b604ed16d3779
Emanuel <emanuelsolis@hotmail.com>
2022-08-12 23:32:58 -0300

    btn agregar

--- public/js/articulos/listar.js
+++ public/js/articulos/listar.js
@@ -1,5 +1,41 @@
 (function ($) {
   $(document).ready(function () {
-   
+    var $lista = [];
+    let url = $("#url").val();
+    let urlReq = url + "apiarticulos/listar";
+    //console.log("url: "+urlReq);
+    //console.log(param);
+    let headers = { "Content-Type": "application/json;charset=utf-8" };
+    let data = {};
+    $.ajax({
+      url: urlReq,
+      headers: headers,
+      type: "GET",
+      type: "POST",
+      data: data,
+      dataType: "json",
+    })
+      .done(function (data) {
+       $lista = data.lista;
+       console.table(lista);
+        /*$listaArticulos = data.datos;
+        console.log($listaArticulos);*/
+      })
+      .fail(function (jqXHR, textStatus, errorThrown) {
+        console.log(textStatus);
+      });
+
+      /*agregar click al boton*/
+
+       $(".btnAgregar").each(function (index) {
+      $(this).on("click", function () {
+        console.log("aprete boton");
+        //let articuloId = this.dataset.articuloId;
+        let articuloId = $(this).data("articuloId");
+        console.log("id:" + articuloId);
+        console.log(articuloId);
+      })
+    })
+
   }); //end ready
 })(jQuery);
