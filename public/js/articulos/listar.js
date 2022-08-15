d3924773d56cee6bac9993b6527f06770e63ff6d
Leandro <Leandro>
2022-08-15 20:37:57 -0300

    agregar items

--- public/js/articulos/listar.js
+++ public/js/articulos/listar.js
@@ -1,6 +1,6 @@
 (function ($) {
   $(document).ready(function () {
-    var $lista = [];
+    let $lista = [];
     let url = $("#url").val();
     let urlReq = url + "apiarticulos/listar";
     let headers = { "Content-Type": "application/json;charset=utf-8" };
@@ -26,7 +26,27 @@
         //console.log("hola");
         let articuloId = this.dataset.articuloId;
         let $id = $(this).data("articuloId");
-        console.log($id);
+        //console.log($id);
+        $articulo = $lista.find((art) => art.id == $id);
+        //console.log($articulo);
+        let cantidad = $("#art-" + $id).val();
+        //console.log(cantidad);
+        item = {
+          id: $id,
+          codigo: $articulo.codigo,
+          descripcion: $articulo.descripcion,
+          precio: $articulo.precio,
+          url: $articulo.url,
+          cantidad: cantidad,
+        };
+        console.log(item);
+        let carritoStr = localStorage.getItem("carrito");
+        let carritoArr = [];
+        if (carritoStr) {
+          carritoArr = JSON.parse(carritoStr);
+        }
+        carritoArr.push(item);
+        localStorage.setItem("carrito", JSON.stringify(carritoArr));
       }); //end item click
     }); //end item click items foreach
 
