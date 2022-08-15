20bdf7c7019b2e2b507da3ad3d41c8a05861c8db
Geromino <gerocgon@gmail.com>
2022-08-15 20:35:10 -0300

    clase 15/08

--- public/js/articulos/listar.js
+++ public/js/articulos/listar.js
@@ -27,6 +27,24 @@
         let articuloId = this.dataset.articuloId;
         let $id = $(this).data("articuloId");
         console.log($id);
+        $articulo = $lista.find((art) => art.id == $id);
+        let cantidad = $("#art-" + $id).val();
+        item = {
+          id: $articulo.id,
+          codigo: $articulo.codigo,
+          descripcion: $articulo.descripcion,
+          precio: $articulo.precio,
+          url: $articulo.url,
+          cantidad: cantidad,
+        };
+        let carritoStr = localStorage.getItem("carrito");
+        let carritoArr = [];
+        if (carritoStr) {
+          carritoArr = JSON.parse(carritoStr);
+        } else {
+          carritoArr.push(item);
+          localStorage.setItem("carrito", JSON.stringify(carritoArr));
+        }
       }); //end item click
     }); //end item click items foreach
 
